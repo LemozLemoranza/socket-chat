72a991cac5010f810c6e1fdd4e279a2e597d914d
Yo <lemoranza@gmail.com>
2022-12-12 21:18:02 -0500

    SOCKE-CHAT

---
+++ public/js/socket-chat-jquery.js
@@ -0,0 +1,126 @@
+const params = new URLSearchParams(window.location.search)
+const nombre = params.get('nombre');
+const sala = params.get('sala');
+
+
+const divUsuarios = $('#divUsuarios');
+const formEnviar = $('#formEnviar');
+const txtMensaje = $('#txtMensaje');
+const divChatbox = $('#divChatbox');
+
+
+
+
+
+
+ function renderizarUsuarios( personas ){
+    console.log(personas);
+    let html = '';
+
+    html += `<li>
+                <a href="javascript:void(0)" class="active"> Chat de <span>${params.get('sala')}</span></a>
+            </li>`;
+
+    for(let i = 0; i < personas.length ; i++){
+
+    html += `<li>
+                <a data-id="${personas[i].id}" href="javascript:void(0)"><img src="assets/images/users/1.jpg" alt="user-img" class="img-circle"> <span>${personas[i].nombre}<small class="text-success">online</small></span></a>
+            </li>`
+        
+    }
+
+    divUsuarios.html(html)    
+ }
+
+
+ function renderizarMensajes( mensaje, yo ){
+   
+    let html = '';
+    let fecha = new Date(mensaje.fecha);
+    let hora = fecha.getHours() + ':' + fecha.getMinutes();
+
+    let admninClass = 'info';
+    if( mensaje.nombre === 'Administrador'){
+        admninClass = 'danger'
+    }
+    if( yo ){
+
+        html = `<li class="reverse">
+        <div class="chat-content">
+        <h5>${mensaje.nombre}</h5>
+        <div class="box bg-light-inverse">${mensaje.mensaje}</div>
+        </div>
+        <div class="chat-img"><img src="assets/images/users/5.jpg" alt="user" /></div>
+        <div class="chat-time">${hora}</div>
+        </li>`
+        
+        
+        
+    }else{
+        
+        
+        html +=  ` <li class='animated fadeIn'>`
+
+        if(mensaje.nombre !== 'Administrador' ){
+            
+            html += `  <div class="chat-img"><img src="assets/images/users/1.jpg" alt="user" /></div>`
+        }
+
+             html +=         ` <div class="chat-content">
+                            <h5>${mensaje.nombre}</h5>
+                            <div class="box bg-light-${admninClass}">${mensaje.mensaje}</div>
+                        </div>
+                        <div class="chat-time">${hora}</div>
+                    </li>`
+    }
+                
+    divChatbox.append(html)
+
+}
+
+
+function scrollBottom() {
+
+    // selectors
+    var newMessage = divChatbox.children('li:last-child');
+
+    // heights
+    var clientHeight = divChatbox.prop('clientHeight');
+    var scrollTop = divChatbox.prop('scrollTop');
+    var scrollHeight = divChatbox.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight() || 0;
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        divChatbox.scrollTop(scrollHeight);
+    }
+}
+
+
+
+ divUsuarios.on('click', 'a', function(){
+    let id = $(this).data('id')
+    if(id){
+        console.log(id)
+    }
+ })
+
+ 
+ formEnviar.on('submit', function(e){
+    e.preventDefault();
+    if( txtMensaje.val().trim().length === 0 ){
+        return
+    }
+
+    
+    socket.emit('crearMensaje', {
+        nombre: nombre,
+        mensaje: txtMensaje.val()
+    }, function(mensaje) {
+        txtMensaje.val('').focus()
+        renderizarMensajes(mensaje, true);
+        scrollBottom()
+    });
+
+
+ })
